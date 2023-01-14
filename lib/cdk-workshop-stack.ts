e2776c37fbaf2528db5f87986d6e70f3428d2287
Yudai Nakajima <kituneudon97@gmail.com>
2023-01-14 12:34:40 +0900

    ec2を構築

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { readFileSync } from 'fs';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -22,6 +23,34 @@ export class CdkWorkshopStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED
         }
       ]
-    })
+    });
+
+    // AMIを設定する
+    const amazonLinux2 = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
+    });
+
+    const userDataScript = readFileSync("./lib/resources/user-data.sh", "utf-8");
+
+    const webSecurityGroup = new ec2.SecurityGroup(this, "WebSG", {
+      vpc: vpc,
+      allowAllOutbound: true
+    });
+
+    // port80, すべてのIPアドレスからのアクセスを許可
+    webSecurityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(80), "Allow inbound HTTP");
+
+    // EC2インスタンスを定義
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      vpc: vpc,
+      securityGroup: webSecurityGroup,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      machineImage: amazonLinux2
+    });
+
+    // ec2インスタンスにユーザーデータを追加
+    webServer1.addUserData(userDataScript);
   }
 }
